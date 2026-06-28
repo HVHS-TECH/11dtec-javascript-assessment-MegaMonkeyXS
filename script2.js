e61bfae8bbd3c5b0fbf788fabf8d62ad4db24c63
MankayFan <24449ds@hvhs.school.nz>
2026-06-28 21:50:30 +0000

    Jun 28, 2026, 9:50 PM

--- script2.js
+++ script2.js
@@ -15,3 +15,59 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 3000); 
 });
 
+const itemSelect = document.getElementById("item");
+const quantityInput = document.getElementById("quantity");
+const totalText = document.getElementById("total");
+const orderForm = document.getElementById("orderForm");
+const ordersDiv = document.getElementById("orders");
+
+function updateTotal() {
+const price = parseInt(
+itemSelect.options[itemSelect.selectedIndex].dataset.price
+);
+
+const quantity = parseInt(quantityInput.value);
+
+totalText.textContent = `Total: $${price * quantity}`;
+}
+
+itemSelect.addEventListener("change", updateTotal);
+quantityInput.addEventListener("input", updateTotal);
+
+orderForm.addEventListener("submit", function(event) {
+event.preventDefault();
+
+const item = itemSelect.value;
+const quantity = quantityInput.value;
+
+const price = parseInt(
+itemSelect.options[itemSelect.selectedIndex].dataset.price
+);
+
+const total = price * quantity;
+
+ordersDiv.innerHTML += `
+<div style="background:white;padding:10px;margin:10px 0;border:2px solid black;">
+<strong>Order Placed!</strong><br>
+Item: ${item}<br>
+Quantity: ${quantity}<br>
+Total: $${total}
+
+<a href="${download}" download
+style="
+display:inline-block;
+margin-top:15px;
+padding:12px 20px;
+background:#4CAF50;
+color:white;
+text-decoration:none;
+border-radius:6px;
+font-weight:bold;
+">
+Download ${item}
+</a>
+</div>
+`;
+});
+
+updateTotal();
